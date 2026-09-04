aff60cc75036a8543ea6d5f38dfc91aac623074c
Seeyan Newaz <seeyannewaz@gmail.com>
2026-09-04 16:25:45 +0000

    feat: Added basic repository testing

---
+++ backend/trading-platform/src/test/java/com/goatteen/trading/user/UserRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.goatteen.trading.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class UserRepositoryTest {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Test
+    void repositoryLoads() {
+
+        assertThat(userRepository).isNotNull();
+
+    }
+
+}
