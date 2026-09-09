4b6c7f47cff484f98ffa9d1a1c39564b10bab1e9
tonnuhongan999 <tonnu.hongan999@gmail.com>
2026-09-09 23:59:13 +0200

    fix: resolve push conflict

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ClientViewComponent } from './client-view/clientview.component';
 
 export const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'dashboard', component: DashboardComponent },
+
+  { path: '', component: AuthComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'dashboard', component: DashboardComponent }
   { path: 'clientview', component: ClientViewComponent }
 ];
