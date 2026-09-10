dab0cd6e3263844d6a0b5c05cd907ed18b202529
William <wliem5741@gmail.com>
2026-09-10 13:22:04 +0000

    feat: nav sidebar added

---
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent {
+  navItems = [
+    {
+      label: 'Dashboard',
+      icon: '📊',
+      route: '/dashboard',
+    },
+    {
+      label: 'Portfolio',
+      icon: '💼',
+      route: '/clientview',
+    },
+  ];
+}
